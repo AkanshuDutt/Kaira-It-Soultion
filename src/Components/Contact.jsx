39f8fe51ee2bf71abb2ecb5a57caf482c73ff487
Akanshu Dutt <duttakanshu@gmail.com>
2025-10-04 18:00:58 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,11 +8,7 @@ const Contact = () => {
   useEffect(() => {
     AOS.init({ duration: 1000 });
   }, []);
-  // {
-  // const Contact = () => {
-  // useEffect(() => {
-  //   AOS.init({ duration: 1000 });
-  // }, []);
+ 
 
   const leftVariant = {
     hidden: { x: -100, opacity: 0 },
